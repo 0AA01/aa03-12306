96f38de747dba631a2d7e91ea54cda6367ecca12
AA <1435220156@qq.com>
2024-07-31 13:51:25 +0800

    feature: 实现分页请求、响应对象类

---
+++ frameworks/convention/src/main/java/com/aa03/index12306/framework/starter/convention/page/PageRequest.java
@@ -0,0 +1,17 @@
+package com.aa03.index12306.framework.starter.convention.page;
+
+/**
+ * 分页请求对象
+ */
+public class PageRequest {
+
+    /**
+     * 当前页
+     */
+    private Long current = 1L;
+
+    /**
+     * 每页显示条数
+     */
+    private Long size = 10L;
+}
