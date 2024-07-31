30c674c0e9d280b08453b95b5e222849b581a5ef
AA <1435220156@qq.com>
2024-07-31 14:57:27 +0800

    feature: 实现初始化调用端口

---
+++ frameworks/web/src/main/java/com/aa03/index12306/framework/starter/web/config/WebAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.aa03.index12306.framework.starter.web.config;
+
+/**
+ * Web 组件自动装配
+ */
+public class WebAutoConfiguration {
+
+    public final static String INITIALIZE_PATH = "/initialize/dispatcher-servlet";
+
+}
