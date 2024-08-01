ee356422b75b91293a003447073bfed2fcfe22f1
AA <1435220156@qq.com>
2024-08-01 21:22:04 +0800

    optimizer: 用户注释

--- services/user-service/src/main/java/com/aa03/index12306/biz/userservice/dao/mapper/UserMapper.java
+++ services/user-service/src/main/java/com/aa03/index12306/biz/userservice/dao/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.aa03.index12306.biz.userservice.dao.mapper;
 
-
 import com.aa03.index12306.biz.userservice.dao.entity.UserDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+/**
+ * 用户信息持久层
+ */
 public interface UserMapper extends BaseMapper<UserDO> {
 }
