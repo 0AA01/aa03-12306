34ea44a5bf3dd25ee2120887195e7b32cfee2740
AA <1435220156@qq.com>
2024-08-01 16:40:37 +0800

    feature: 实现幂等组件模块

---
+++ frameworks/idempotent/src/main/java/com/aa03/index12306/framework/starter/idempotent/core/param/IdempotentParamService.java
@@ -0,0 +1,9 @@
+package com.aa03.index12306.framework.starter.idempotent.core.param;
+
+import com.aa03.index12306.framework.starter.idempotent.core.IdempotentExecuteHandler;
+
+/**
+ * 参数方式幂等实现接口
+ */
+public interface IdempotentParamService extends IdempotentExecuteHandler {
+}
---
+++ frameworks/idempotent/src/main/java/com/aa03/index12306/framework/starter/idempotent/core/spel/IdempotentSpELService.java
@@ -0,0 +1,9 @@
+package com.aa03.index12306.framework.starter.idempotent.core.spel;
+
+import com.aa03.index12306.framework.starter.idempotent.core.IdempotentExecuteHandler;
+
+/**
+ * SpEL 方式幂等实现接口
+ */
+public interface IdempotentSpELService extends IdempotentExecuteHandler {
+}
