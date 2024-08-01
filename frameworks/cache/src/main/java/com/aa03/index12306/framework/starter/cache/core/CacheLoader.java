1379c1cd6af68a7dacc2c45e52a88d313b7ba321
AA <1435220156@qq.com>
2024-08-01 15:26:54 +0800

    feature: 实现缓存组件模块

---
+++ frameworks/cache/src/main/java/com/aa03/index12306/framework/starter/cache/core/CacheLoader.java
@@ -0,0 +1,13 @@
+package com.aa03.index12306.framework.starter.cache.core;
+
+/**
+ * 缓存加载器
+ */
+@FunctionalInterface
+public interface CacheLoader<T> {
+
+    /**
+     * 加载缓存
+     */
+    T load();
+}
