a51b6a1312c0127faa679d97ac11ae11db92a114
AA <1435220156@qq.com>
2024-08-02 21:16:07 +0800

    feature: 实现车站站点信息查询功能

---
+++ services/ticket-service/src/main/java/com/aa03/index12306/biz/ticketservice/dao/mapper/StationMapper.java
@@ -0,0 +1,10 @@
+package com.aa03.index12306.biz.ticketservice.dao.mapper;
+
+import com.aa03.index12306.biz.ticketservice.dao.entity.StationDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 车站持久层
+ */
+public interface StationMapper extends BaseMapper<StationDO> {
+}
