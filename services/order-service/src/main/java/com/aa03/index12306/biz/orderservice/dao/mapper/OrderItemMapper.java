9a400061f885967d70566d40f939a7ff4a46b501
AA <1435220156@qq.com>
2024-08-06 19:50:25 +0800

    feature: 订单表、详细订单表、订单证件号关联表实体类和持久层实现

---
+++ services/order-service/src/main/java/com/aa03/index12306/biz/orderservice/dao/mapper/OrderItemMapper.java
@@ -0,0 +1,10 @@
+package com.aa03.index12306.biz.orderservice.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.aa03.index12306.biz.orderservice.dao.entity.OrderItemDO;
+
+/**
+ * 订单明细持久层
+ */
+public interface OrderItemMapper extends BaseMapper<OrderItemDO> {
+}
---
+++ services/order-service/src/main/java/com/aa03/index12306/biz/orderservice/dao/mapper/OrderItemPassengerMapper.java
@@ -0,0 +1,10 @@
+package com.aa03.index12306.biz.orderservice.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.aa03.index12306.biz.orderservice.dao.entity.OrderItemPassengerDO;
+
+/**
+ * 乘车人订单关系持久层
+ */
+public interface OrderItemPassengerMapper extends BaseMapper<OrderItemPassengerDO> {
+}
